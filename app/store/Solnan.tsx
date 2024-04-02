d79eb62756a89e07b6156ab93e134847609c970b
MediconcenVictor <victor_chang@mediconcen.com>
2024-04-02 11:25:05 +0800

    Remove unused import statement in Solnan.tsx

--- app/store/Solnan.tsx
+++ app/store/Solnan.tsx
@@ -6,7 +6,6 @@ import {
 } from "@solana/wallet-adapter-react";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import * as web3 from "@solana/web3.js";
-import * as walletAdapterWallets from "@solana/wallet-adapter-wallets";
 require("@solana/wallet-adapter-react-ui/styles.css");
 
 const SolanaWalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
